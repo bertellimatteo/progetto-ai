49f116864ea5316555ace1424f356dcd5d383e71
bertellimatteo <lavoro.bertellimatteo@gmail.com>
2025-01-06 17:31:14 +0100

    fix the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,10 @@
-import { AppSidebar } from "@/components/(sidebar)/app-sidebar"
-
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
-
-import { Header } from "@/components/(header)/app-header"
-
-import { Footer } from "@/components/(footer)/app-footer"
-
-
-
-export default function Page() {
+// app/page.tsx
+export default function HomePage() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-
-       
-        <div className="flex flex-col m-2 ml-0 h-full justify-between gap-2">
-          <Header />
-
-          <main className="bg-red-50 flex-1"> </main>
-      
-          <Footer />
-
-        </div>
-    
-        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div> */}
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <h1 className="text-2xl font-bold">Benvenuto!</h1>
+      <p>Qui puoi inserire le card, i form, ecc.</p>
+    </>
   )
 }
+
